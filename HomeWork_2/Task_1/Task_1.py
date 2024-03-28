c6f82cffa814f81f5eecbf60852e99cc7f358a2e
Andrei <andrei.yakimyuk>
2024-03-28 19:59:35 +0800

    HW-2 Task-1

--- HomeWork_2/Task_1/Task_1.py
+++ HomeWork_2/Task_1/Task_1.py
@@ -15,3 +15,31 @@
 # coins = [0, 1, 0, 1, 1, 0]
 # На выходе:
 # 3
+
+# main/ver
+# coins = [0, 1, 0, 1, 1, 0]
+# orel, reshka = 0, 0
+# for i in coins:
+#     if i == 0:
+#         orel += 1
+#     elif i == 1: 
+#         reshka += 1       
+# if orel <= reshka:
+#     print(orel)
+# else:
+#     print(reshka)
+
+# версия автотеста
+# count_zero = 0
+# count_one = 0
+
+# for coin in coins:
+#     if coin == 0:
+#         count_zero += 1
+#     else:
+#         count_one += 1
+
+# if count_one > count_zero:
+#     print(count_zero)
+# else:
+#     print(count_one)
